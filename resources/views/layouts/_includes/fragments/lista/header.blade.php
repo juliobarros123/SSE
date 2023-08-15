2c0668308d411bd660028722b3c979b02a6ee4d2
juliobarros123 <filoarte0@gmail.com>
2023-08-15 12:08:00 +0100

    u

--- resources/views/layouts/_includes/fragments/lista/header.blade.php
+++ resources/views/layouts/_includes/fragments/lista/header.blade.php
@@ -1,5 +1,6 @@
 <div class="header">
-    <img src="{{ $cabecalho->vc_logo }}" alt="Logo da Escola">
+    <img src="{{ $cabecalho->vc_logo }}" alt="Logo da Escola" style="height: 50px;
+    width: 107px;">
    
   
     <h3>{{$cabecalho->vc_republica}} <br>{{$cabecalho->vc_ministerio}} 
